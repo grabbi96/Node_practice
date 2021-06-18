21a5c0ae6262cafc96ee6f92e0c6fc23cfca4584
grabbi96 <grabbi96@gmail.com>
2021-06-18 12:14:28 +0600

    :ok_hand: FEAT: new route added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("IT Woekdedd");
 });
 
+app.get("/new", (req, res) => {
+  res.send("IT  Woekdedd new new");
+});
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
